8abef1373d1df592bdae9ad7a64cd0a2fab4d175
Aaron Murray <aaron@omv-extras.org>
2013-12-18 09:54:43 -0600

    initial commit

---
+++ var/www/openmediavault/js/omv/module/admin/service/roundcube/Settings.js
@@ -0,0 +1,254 @@
+/**
+ *
+ * @license http://www.gnu.org/licenses/gpl.html GPL Version 3
+ * @copyright Copyright (c) 2013 OpenMediaVault Plugin Developers
+ *
+ * This file is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or any later version.
+ *
+ * This file is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this file. If not, see <http://www.gnu.org/licenses/>.
+ */
+// require("js/omv/WorkspaceManager.js")
+// require("js/omv/workspace/form/Panel.js")
+// require("js/omv/form/field/SharedFolderComboBox.js")
+
+Ext.define("OMV.module.admin.service.roundcube.Settings", {
+    extend : "OMV.workspace.form.Panel",
+
+    plugins: [{
+        ptype        : "linkedfields",
+        correlations : [{
+            name       : [
+                "launch-webmail-site"
+            ],
+            conditions : [{
+                name  : "enable",
+                value : true
+            }],
+            properties : "enabled"
+        },{
+            name       : [
+                "installdb"
+            ],
+            conditions : [{
+                name  : "enable",
+                value : false 
+            }],
+            properties : "!disabled"
+        }]
+    }],
+
+    initComponent : function () {
+        var me = this;
+
+        me.on('load', function () {
+            var checked = me.findField('enable').checked;
+            var showtab = me.findField('showtab').checked;
+            var parent = me.up('tabpanel');
+
+            if (!parent)
+                return;
+
+            var webmailPanel = parent.down('panel[title=' + _("Webmail") + ']');
+
+            if (webmailPanel) {
+                checked ? webmailPanel.enable() : webmailPanel.disable();
+                showtab ? webmailPanel.tab.show() : webmailPanel.tab.hide();
+            }
+        });
+
+        me.callParent(arguments);
+    },
+    
+    rpcService   : "Roundcube",
+    rpcGetMethod : "getSettings",
+    rpcSetMethod : "setSettings",
+
+    getFormItems : function() {
+        return [{
+            xtype    : "fieldset",
+            title    : _("General settings"),
+            defaults : {
+                labelSeparator:""
+            },
+            items    : [{
+                xtype      : "checkbox",
+                name       : "enable",
+                fieldLabel : _("Enable"),
+                checked    : false
+            },{
+                xtype      : "textfield",
+                name       : "default_host",
+                fieldLabel : _("Default Host"),
+                allowBlank : true,
+                plugins    : [{
+                    ptype : "fieldinfo",
+                    text  : _("The mail host chosen to perform the log-in. Leave blank to show a textbox at login.")
+                }]
+            },{
+                xtype      : "textfield",
+                name       : "smtp_server",
+                fieldLabel : _("SMTP Server"),
+                allowBlank : false
+            },{
+                xtype      : "numberfield",
+                name       : "smtp_port",
+                fieldLabel : _("SMTP Port"),
+                inputValue : 25
+            },{
+                xtype      : "textfield",
+                name       : "smtp_user",
+                fieldLabel : _("SMTP User"),
+                allowBlank : true,
+                plugins    : [{
+                    ptype : "fieldinfo",
+                    text  : _("If required. Use %u as the username and Roundcube will use the current username for login.")
+                }]
+            },{
+                xtype      : "textfield",
+                name       : "smtp_pass",
+                fieldLabel : _("SMTP Password"),
+                allowBlank : true,
+                plugins    : [{
+                    ptype : "fieldinfo",
+                    text  : _("If required. Use %p as the password and Roundcube will use the current user's password for login.")
+                }]
+            },{
+                xtype      : "textfield",
+                name       : "product_name",
+                fieldLabel : _("Product Name"),
+                allowBlank : false
+            },{
+                xtype      : "passwordfield",
+                name       : "des_key",
+                fieldLabel : _("DES Key"),
+                allowBlank : false,
+                minLength  : 24,
+                maxLength  : 24,
+                plugins    : [{
+                    ptype : "fieldinfo",
+                    text  : _("Random key created at installation.  Must be 24 characters long.")
+                }]
+            }]
+        },{
+            xtype    : "fieldset",
+            title    : _("Database Settings"),
+            defaults : {
+                labelSeparator : ""
+            },
+            items    : [{
+                xtype      : "textfield",
+                name       : "db_host",
+                fieldLabel : _("Hostname"),
+                allowBlank : false
+            },{
+                xtype      : "textfield",
+                name       : "db_name",
+                fieldLabel : _("Database Name"),
+                allowBlank : false
+            },{
+                xtype      : "textfield",
+                name       : "db_user",
+                fieldLabel : _("Username"),
+                allowBlank : false
+            },{
+                xtype      : "passwordfield",
+                name       : "db_pass",
+                fieldLabel : _("Password"),
+                allowBlank : false
+            },{
+                xtype      : "passwordfield",
+                name       : "root_pass",
+                fieldLabel : _("MySQL root Password"),
+                allowBlank : true,
+                plugins    : [{
+                    ptype : "fieldinfo",
+                    text  : _("Used only for installing Roundcube database and will not be saved.")
+                }]
+            },{
+                border : false,
+                html   : "<br />"
+            },{
+                xtype   : "button",
+                name    : "installdb",
+                text    : _("Install DB"),
+                scope   : this,
+                handler : function() {
+                    var me = this;
+                    OMV.MessageBox.show({
+                        title   : _("Confirmation"),
+                        msg     : _("Are you sure you want to install the Roundcube database?"),
+                        buttons : Ext.Msg.YESNO,
+                        fn      : function(answer) {
+                            if (answer !== "yes")
+                               return;
+
+                            OMV.MessageBox.wait(null, _("Installing Roundcube database"));
+                            OMV.Rpc.request({
+                                scope   : me,
+                                rpcData : {
+                                    service : "Roundcube",
+                                    method  : "doInstallDB",
+                                    params  : {
+                                        db_host   : me.getForm().findField("db_host").getValue(),
+                                        db_name   : me.getForm().findField("db_name").getValue(),
+                                        db_user   : me.getForm().findField("db_user").getValue(),
+                                        db_pass   : me.getForm().findField("db_pass").getValue(),
+                                        root_pass : me.getForm().findField("root_pass").getValue()
+                                    }
+                                },
+                                success : function(id, success, response) {
+                                    me.doReload();
+                                    OMV.MessageBox.hide();
+                                }
+                            });
+                        },
+                        scope : me,
+                        icon  : Ext.Msg.QUESTION
+                    });
+                }
+            },{
+                border : false,
+                html   : "<br />"
+            }]
+        },{
+            xtype    : "fieldset",
+            title    : _("Webmail site"),
+            defaults : {
+                labelSeparator : ""
+            },
+            items : [{
+                xtype      : "checkbox",
+                name       : "showtab",
+                fieldLabel : _("Enable"),
+                boxLabel   : _("Show tab containing webmail frame."),
+                checked    : false
+            },{
+                xtype      : "button",
+                name       : "launch-webmail-site",
+                text       : _("Launch webmail site"),
+                disabled   : true,
+                handler    : function() {
+                    window.open("/webmail/");
+                }
+            },{
+                border: false,
+                html: "<br />"                
+            }]
+        }];
+    }
+});
+
+OMV.WorkspaceManager.registerPanel({
+    id: "settings",
+    path: "/service/roundcube",
+    text: _("Settings"),
+    position: 10,
+    className: "OMV.module.admin.service.roundcube.Settings"
+});
---
+++ var/www/roundcube/plugins/managesieve/managesieve.php
@@ -0,0 +1,202 @@
+<?php
+
+/**
+ * Managesieve (Sieve Filters)
+ *
+ * Plugin that adds a possibility to manage Sieve filters in Thunderbird's style.
+ * It's clickable interface which operates on text scripts and communicates
+ * with server using managesieve protocol. Adds Filters tab in Settings.
+ *
+ * @version @package_version@
+ * @author Aleksander Machniak <alec@alec.pl>
+ *
+ * Configuration (see config.inc.php.dist)
+ *
+ * Copyright (C) 2008-2013, The Roundcube Dev Team
+ * Copyright (C) 2011-2013, Kolab Systems AG
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ */
+
+class managesieve extends rcube_plugin
+{
+    public $task = 'mail|settings';
+    private $rc;
+    private $engine;
+
+    function init()
+    {
+        $this->rc = rcmail::get_instance();
+
+        // register actions
+        $this->register_action('plugin.managesieve', array($this, 'managesieve_actions'));
+        $this->register_action('plugin.managesieve-action', array($this, 'managesieve_actions'));
+        $this->register_action('plugin.managesieve-save', array($this, 'managesieve_save'));
+
+        if ($this->rc->task == 'settings') {
+            $this->init_ui();
+        }
+        else if ($this->rc->task == 'mail') {
+            // register message hook
+            $this->add_hook('message_headers_output', array($this, 'mail_headers'));
+
+            // inject Create Filter popup stuff
+            if (empty($this->rc->action) || $this->rc->action == 'show') {
+                $this->mail_task_handler();
+            }
+        }
+    }
+
+    /**
+     * Initializes plugin's UI (localization, js script)
+     */
+    function init_ui()
+    {
+        if ($this->ui_initialized) {
+            return;
+        }
+
+        // load localization
+        $this->add_texts('localization/', array('filters','managefilters'));
+        $this->include_script('managesieve.js');
+
+        $this->ui_initialized = true;
+    }
+
+    /**
+     * Add UI elements to the 'mailbox view' and 'show message' UI.
+     */
+    function mail_task_handler()
+    {
+        // make sure we're not in ajax request
+        if ($this->rc->output->type != 'html') {
+            return;
+        }
+
+        // use jQuery for popup window
+        $this->require_plugin('jqueryui');
+
+        // include js script and localization
+        $this->init_ui();
+
+        // include styles
+        $skin_path = $this->local_skin_path();
+        if (is_file($this->home . "/$skin_path/managesieve_mail.css")) {
+            $this->include_stylesheet("$skin_path/managesieve_mail.css");
+        }
+
+        // add 'Create filter' item to message menu
+        $this->api->add_content(html::tag('li', null, 
+            $this->api->output->button(array(
+                'command'  => 'managesieve-create',
+                'label'    => 'managesieve.filtercreate',
+                'type'     => 'link',
+                'classact' => 'icon filterlink active',
+                'class'    => 'icon filterlink',
+                'innerclass' => 'icon filterlink',
+            ))), 'messagemenu');
+
+        // register some labels/messages
+        $this->rc->output->add_label('managesieve.newfilter', 'managesieve.usedata',
+            'managesieve.nodata', 'managesieve.nextstep', 'save');
+
+        $this->rc->session->remove('managesieve_current');
+    }
+
+    /**
+     * Get message headers for popup window
+     */
+    function mail_headers($args)
+    {
+        // this hook can be executed many times
+        if ($this->mail_headers_done) {
+            return $args;
+        }
+
+        $this->mail_headers_done = true;
+
+        $headers = $args['headers'];
+        $ret     = array();
+
+        if ($headers->subject)
+            $ret[] = array('Subject', rcube_mime::decode_header($headers->subject));
+
+        // @TODO: List-Id, others?
+        foreach (array('From', 'To') as $h) {
+            $hl = strtolower($h);
+            if ($headers->$hl) {
+                $list = rcube_mime::decode_address_list($headers->$hl);
+                foreach ($list as $item) {
+                    if ($item['mailto']) {
+                        $ret[] = array($h, $item['mailto']);
+                    }
+                }
+            }
+        }
+
+        if ($this->rc->action == 'preview')
+            $this->rc->output->command('parent.set_env', array('sieve_headers' => $ret));
+        else
+            $this->rc->output->set_env('sieve_headers', $ret);
+
+
+        return $args;
+    }
+
+    /**
+     * Plugin action handler
+     */
+    function managesieve_actions()
+    {
+        $this->init_ui();
+        $engine = $this->get_engine();
+        $engine->actions();
+    }
+
+    /**
+     * Forms save action handler
+     */
+    function managesieve_save()
+    {
+        // load localization
+        $this->add_texts('localization/', array('filters','managefilters'));
+
+        // include main js script
+        if ($this->api->output->type == 'html') {
+            $this->include_script('managesieve.js');
+        }
+
+        $engine = $this->get_engine();
+        $engine->save();
+    }
+
+    /**
+     * Initializes engine object
+     */
+    private function get_engine()
+    {
+        if (!$this->engine) {
+            $this->load_config();
+
+            // Add include path for internal classes
+            $include_path = $this->home . '/lib' . PATH_SEPARATOR;
+            $include_path .= ini_get('include_path');
+            set_include_path($include_path);
+
+            $this->engine = new rcube_sieve_engine($this);
+        }
+
+        return $this->engine;
+    }
+}
